ef00b163b5b2f9292ebb31870c877a1deab89c3c
SigwiLi <87345027+SigwiLi@users.noreply.github.com>
2025-12-05 15:45:10 -0600

    Added AutoEncoder.py

---
+++ autoEncoder.py
@@ -0,0 +1,100 @@
+import torch
+from torch import nn, optim
+from torchvision import datasets, transforms
+import matplotlib.pyplot as plt
+
+
+# Load MNIST Dataset
+tensor_transform = transforms.ToTensor()
+dataset = datasets.MNIST(root="./data", train=True,
+                         download=True, transform=tensor_transform)
+loader = torch.utils.data.DataLoader(
+    dataset=dataset, batch_size=32, shuffle=True)
+
+# Define AutoEncoder class
+class AE(nn.Module):
+    def __init__(self):
+        super(AE, self).__init__()
+        # Encoder - Compresses Images, using Linear Layers and ReLU activations,
+        # into embedding space
+        self.encoder = nn.Sequential(
+            nn.Linear(28 * 28, 128),
+            nn.ReLU(),
+            nn.Linear(128, 64),
+            nn.ReLU(),
+            nn.Linear(64, 36),
+            nn.ReLU(),
+            nn.Linear(36, 18),
+            nn.ReLU(),
+            nn.Linear(18, 9)
+        )
+        # Decoder - Reconstructs Images using Linear Layers and ReLU activations and Sigmoid to output values
+        # between 0 and 1
+        self.decoder = nn.Sequential(
+            nn.Linear(9,18),
+            nn.ReLU(),
+            nn.Linear(18,36),
+            nn.ReLU(),
+            nn.Linear(36,64),
+            nn.ReLU(),
+            nn.Linear(64,128),
+            nn.ReLU(),
+            nn.Linear(128, 28 * 28),
+            nn.Sigmoid()
+        )
+    # Forward Propagation
+    def forward(self, x):
+        encoded = self.encoder(x)
+        decoded = self.decoder(encoded)
+        return decoded
+    
+    
+# Load model, loss function(Mean Squared) and optimizer
+model = AE()
+loss_function = nn.MSELoss()
+optimizer = optim.Adam(model.parameters(), lr=1e-3, weight_decay=1e-8)
+
+epochs = 20
+outputs = []
+losses = []
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model.to(device)
+
+# Training Loop 
+for epoch in range(epochs):
+    for images, _ in loader:
+        images = images.view(-1, 28 * 28).to(device)
+        reconstructed = model(images)
+        loss = loss_function(reconstructed, images)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        losses.append(loss.item())
+    outputs.append((epochs, images, reconstructed))
+    print(f"Epoch:{epoch + 1}, Loss:{loss.item():.4f}")
+
+
+plt.style.use('fivethirtyeight')
+plt.figure(figsize=(8, 5))
+plt.plot(losses, label='Loss')
+plt.xlabel('Iterations')
+plt.ylabel('Loss')
+plt.legend()
+plt.show()
+
+model.eval()
+dataiter = iter(loader)
+images, _ = next(dataiter)
+
+images = images.view(-1, 28 * 28).to(device)
+reconstructed = model(images)
+
+fig, axes = plt.subplots(nrows=2, ncols=10, figsize=(10, 3))
+for i in range(10):
+    axes[0, i].imshow(images[i].cpu().detach().numpy().reshape(28, 28), cmap='gray')
+    axes[0, i].axis('off')
+    axes[1, i].imshow(reconstructed[i].cpu().detach().numpy().reshape(28, 28), cmap='gray')
+    axes[1, i].axis('off')
+plt.show()
